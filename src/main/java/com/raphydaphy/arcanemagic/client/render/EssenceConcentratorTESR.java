e9339d16ebb4c13b47503300c8582ba85f78db48
Shadows_of_Fire <Bward7864@gmail.com>
2017-11-17 01:02:33 -0500

    the better rotate

--- src/main/java/com/raphydaphy/arcanemagic/client/render/EssenceConcentratorTESR.java
+++ src/main/java/com/raphydaphy/arcanemagic/client/render/EssenceConcentratorTESR.java
@@ -12,12 +12,9 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class EssenceConcentratorTESR extends TileEntitySpecialRenderer<TileEntityEssenceConcentrator>
-{
+public class EssenceConcentratorTESR extends TileEntitySpecialRenderer<TileEntityEssenceConcentrator> {
 	@Override
-	public void render(TileEntityEssenceConcentrator te, double x, double y, double z, float partialTicks,
-			int destroyStage, float alpha)
-	{
+	public void render(TileEntityEssenceConcentrator te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
 		GlStateManager.pushMatrix();
 		GlStateManager.translate(x, y, z);
 		GlStateManager.disableRescaleNormal();
@@ -27,18 +24,19 @@ public class EssenceConcentratorTESR extends TileEntitySpecialRenderer<TileEntit
 		GlStateManager.popMatrix();
 	}
 
-	private void renderItem(TileEntityEssenceConcentrator te)
-	{
+	private int myFather = 0;
+
+	private void renderItem(TileEntityEssenceConcentrator te) {
 		ItemStack stack = te.getStack();
-		if (!stack.isEmpty())
-		{
+		if (!stack.isEmpty()) {
 			RenderHelper.enableStandardItemLighting();
 			GlStateManager.enableLighting();
 			GlStateManager.pushMatrix();
 			GlStateManager.translate(.5, 0.935, .5);
 			GlStateManager.scale(.2f, .2f, .2f);
-			GlStateManager.rotate(te.getAge() * 1.5f, 0, 1, 0);
-			GlStateManager.translate(0, Math.sin(0.2 * (te.getAge() / 2)) / 4, 0);
+			GlStateManager.rotate(myFather, 0, 1, 0);
+			GlStateManager.translate(0, Math.sin((Math.PI / 180) * (myFather++)) / 3.2 + 0.1, 0);
+			if(myFather < 0 || myFather == Integer.MAX_VALUE) myFather = 0;
 			Minecraft.getMinecraft().getRenderItem().renderItem(stack, ItemCameraTransforms.TransformType.NONE);
 			GlStateManager.popMatrix();
 		}
