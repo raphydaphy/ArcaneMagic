44d478d62212892771fc72ab63ba53522627b9ba
RAPH HENNESSY <raph.hennessy@gmail.com>
2017-11-17 16:30:55 +1100

    flafer pls 2

--- src/main/java/com/raphydaphy/arcanemagic/client/render/GLHelper.java
+++ src/main/java/com/raphydaphy/arcanemagic/client/render/GLHelper.java
@@ -553,7 +553,7 @@ public class GLHelper
 			defaultColor = new Color((int) r, (int) g, (int) b);
 		}
 
-		GLHelper.renderFancyBeams(pos.x, pos.y, pos.z, defaultColor, seed, continuousTick, 16, 0.5f, 30, 10);
+		GLHelper.renderFancyBeams(pos.x + 0.5, pos.y, pos.z + 0.5, defaultColor, seed, continuousTick, 16, 0.5f, 30, 10);
 		GlStateManager.popAttrib();
 		GlStateManager.popMatrix();
 
