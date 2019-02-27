8627906b52afe3c562350510f99b532856f4b12a
Raph Hennessy <raph.hennessy@gmail.com>
2019-02-27 18:39:23 +1100

    move fluid logic into mixer for now

--- src/main/java/com/raphydaphy/arcanemagic/block/entity/MixerBlockEntity.java
+++ src/main/java/com/raphydaphy/arcanemagic/block/entity/MixerBlockEntity.java
@@ -1,22 +1,29 @@
 package com.raphydaphy.arcanemagic.block.entity;
 
-import com.raphydaphy.arcanemagic.block.DoubleBlockBase;
-import com.raphydaphy.arcanemagic.block.MixerBlock;
 import com.raphydaphy.arcanemagic.init.ModRegistry;
 import com.raphydaphy.arcanemagic.network.ArcaneMagicPacketHandler;
 import com.raphydaphy.arcanemagic.network.ClientBlockEntityUpdatePacket;
 import com.raphydaphy.arcanemagic.util.ArcaneMagicUtils;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.enums.DoubleBlockHalf;
+import io.github.prospector.silk.fluid.DropletValues;
+import io.github.prospector.silk.fluid.FluidContainer;
+import io.github.prospector.silk.fluid.FluidInstance;
 import net.minecraft.client.network.packet.BlockEntityUpdateS2CPacket;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.inventory.SidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.Tickable;
 import net.minecraft.util.math.Direction;
 
-public class MixerBlockEntity extends DoubleBlockEntity implements SidedInventory, Tickable
+public class MixerBlockEntity extends DoubleBlockEntity implements SidedInventory, Tickable, FluidContainer
 {
+	private static final String WATER_KEY = "Water";
+	private static final String LIQUIFIED_SOUL_KEY = "LiquifiedSoul";
+	private static final int MAX_FLUID = DropletValues.BUCKET * 4;
+
+	private FluidInstance water = FluidInstance.EMPTY;
+	private FluidInstance liquified_soul = FluidInstance.EMPTY;
 	public long ticks = 0;
 
 	private final int[] slots = { 0 };
@@ -38,6 +45,113 @@ public class MixerBlockEntity extends DoubleBlockEntity implements SidedInventor
 			bottom = ArcaneMagicUtils.isBottomBlock(world, pos, ModRegistry.MIXER);
 			setBottom = true;
 		}
+	}
+
+	@Override
+	public void fromTag(CompoundTag tag)
+	{
+		super.fromTag(tag);
+		if (tag.containsKey(WATER_KEY))
+		{
+			water = new FluidInstance((CompoundTag)tag.getTag(WATER_KEY));
+		} else
+		{
+			water = FluidInstance.EMPTY;
+		}
+		if (tag.containsKey(LIQUIFIED_SOUL_KEY))
+		{
+			liquified_soul = new FluidInstance((CompoundTag)tag.getTag(LIQUIFIED_SOUL_KEY));
+		} else
+		{
+			liquified_soul = FluidInstance.EMPTY;
+		}
+	}
+
+	@Override
+	public CompoundTag toTag(CompoundTag tag)
+	{
+		super.toTag(tag);
+		if (!water.isEmpty())
+		{
+			CompoundTag waterTag = new CompoundTag();
+			water.toTag(waterTag);
+			tag.put(WATER_KEY, waterTag);
+		}
+		if (!liquified_soul.isEmpty())
+		{
+			CompoundTag liquifiedSoulTag = new CompoundTag();
+			liquified_soul.toTag(liquifiedSoulTag);
+			tag.put(LIQUIFIED_SOUL_KEY, liquifiedSoulTag);
+		}
+		return tag;
+	}
+
+	@Override
+	public int getMaxCapacity()
+	{
+		return MAX_FLUID;
+	}
+
+	public boolean canInsertFluidBottom(boolean bottom, Direction fromSide, Fluid fluid, int amount)
+	{
+		return fluid == Fluids.WATER && !bottom && water.getAmount() + amount <= MAX_FLUID;
+	}
+
+	@Override
+	public boolean canInsertFluid(Direction fromSide, Fluid fluid, int amount)
+	{
+		if (!bottom)
+		{
+			DoubleBlockEntity bottomBlockEntity = getBottom();
+			if (bottomBlockEntity instanceof MixerBlockEntity)
+			{
+				return ((MixerBlockEntity) bottomBlockEntity).canInsertFluidBottom(bottom, fromSide, fluid, amount);
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean canExtractFluid(Direction fromSide, Fluid fluid, int amount)
+	{
+		return bottom && fluid == ModRegistry.LIQUIFIED_SOUL && this.liquified_soul.getAmount() + amount <= MAX_FLUID;
+	}
+
+	@Override
+	public void insertFluid(Direction fromSide, Fluid fluid, int amount)
+	{
+
+	}
+
+	@Override
+	public void extractFluid(Direction fromSide, Fluid fluid, int amount)
+	{
+		if (!world.isClient && this.liquified_soul.getFluid() == fluid)
+		{
+			this.liquified_soul.subtractAmount(amount);
+
+			if (this.liquified_soul.getAmount() == 0)
+			{
+				this.liquified_soul = FluidInstance.EMPTY;
+			}
+			markDirty();
+		}
+	}
+
+	@Override
+	public void setFluid(Direction fromSide, FluidInstance instance)
+	{
+		if (!world.isClient)
+		{
+			this.liquified_soul = instance;
+			markDirty();
+		}
+	}
+
+	@Override
+	public FluidInstance[] getFluids(Direction fromSide)
+	{
+		return new FluidInstance[]{liquified_soul};
 	}
 
 	@Override
--- src/main/java/com/raphydaphy/arcanemagic/block/entity/PumpBlockEntity.java
+++ src/main/java/com/raphydaphy/arcanemagic/block/entity/PumpBlockEntity.java
@@ -1,104 +1,12 @@
 package com.raphydaphy.arcanemagic.block.entity;
 
 import com.raphydaphy.arcanemagic.init.ModRegistry;
-import io.github.prospector.silk.fluid.DropletValues;
-import io.github.prospector.silk.fluid.FluidContainer;
-import io.github.prospector.silk.fluid.FluidInstance;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.fluid.Fluid;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.util.math.Direction;
 
-public class PumpBlockEntity extends BlockEntity implements FluidContainer
+public class PumpBlockEntity extends BlockEntity
 {
-	private static final int MAX_FLUID = DropletValues.BUCKET * 4;
-	private FluidInstance fluid = FluidInstance.EMPTY;
-
 	public PumpBlockEntity()
 	{
 		super(ModRegistry.PUMP_TE);
-	}
-
-	@Override
-	public void fromTag(CompoundTag tag)
-	{
-		super.fromTag(tag);
-		if (tag.containsKey(FluidInstance.FLUID_KEY))
-		{
-			fluid = new FluidInstance(tag);
-		} else
-		{
-			fluid = FluidInstance.EMPTY;
-		}
-	}
-
-	@Override
-	public CompoundTag toTag(CompoundTag tag)
-	{
-		super.toTag(tag);
-		if (!fluid.isEmpty())
-		{
-			fluid.toTag(tag);
-		}
-		return tag;
-	}
-
-	@Override
-	public int getMaxCapacity()
-	{
-		return MAX_FLUID;
-	}
-
-	@Override
-	public boolean canInsertFluid(Direction fromSide, Fluid fluid, int amount)
-	{
-		return false;
-	}
-
-	@Override
-	public boolean canExtractFluid(Direction fromSide, Fluid fluid, int amount)
-	{
-		if (this.fluid.isEmpty() || this.fluid.getFluid() == fluid)
-		{
-			return this.fluid.getAmount() + amount <= MAX_FLUID;
-		}
-		return false;
-	}
-
-	@Override
-	public void insertFluid(Direction fromSide, Fluid fluid, int amount)
-	{
-
-	}
-
-	@Override
-	public void extractFluid(Direction fromSide, Fluid fluid, int amount)
-	{
-		if (!world.isClient && this.fluid.getFluid() == fluid)
-		{
-			this.fluid.subtractAmount(amount);
-
-			if (this.fluid.getAmount() == 0)
-			{
-				this.fluid = FluidInstance.EMPTY;
-			}
-			markDirty();
-		}
-	}
-
-	@Override
-	public void setFluid(Direction fromSide, FluidInstance instance)
-	{
-		if (!world.isClient)
-		{
-			this.fluid = instance;
-			markDirty();
-		}
-	}
-
-	@Override
-	public FluidInstance[] getFluids(Direction fromSide)
-	{
-		return new FluidInstance[]{fluid};
 	}
 }
