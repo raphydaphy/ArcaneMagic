44ac8d0c2bcc1ba384c13b44a746962028ada4f7
Raph Hennessy <raph.hennessy@gmail.com>
2019-03-03 10:18:53 +1100

    Coal, Gold and Redstone passive bonuses

--- src/main/java/com/raphydaphy/arcanemagic/core/ItemStackMixin.java
+++ src/main/java/com/raphydaphy/arcanemagic/core/ItemStackMixin.java
@@ -3,7 +3,9 @@ package com.raphydaphy.arcanemagic.core;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import com.raphydaphy.arcanemagic.init.ArcaneMagicConstants;
+import com.raphydaphy.arcanemagic.item.CrystalToolItem;
 import com.raphydaphy.arcanemagic.item.DaggerItem;
+import com.raphydaphy.arcanemagic.item.ICrystalEquipment;
 import com.raphydaphy.arcanemagic.util.ArcaneMagicUtils;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
@@ -17,27 +19,40 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.UUID;
-
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin
 {
-	@Shadow public abstract CompoundTag getTag();
+	@Shadow
+	public abstract CompoundTag getTag();
 
-	@Shadow public abstract Item getItem();
+	@Shadow
+	public abstract Item getItem();
 
-	@Inject(at = @At("RETURN"), method="getAttributeModifiers", cancellable=true)
+	@Inject(at = @At("RETURN"), method = "getDurability", cancellable = true)
+	private void getDurability(CallbackInfoReturnable<Integer> info)
+	{
+		if (getItem() instanceof ICrystalEquipment && getTag() != null)
+		{
+			ArcaneMagicUtils.ForgeCrystal passive = ArcaneMagicUtils.ForgeCrystal.getFromID(getTag().getString(ArcaneMagicConstants.PASSIVE_CRYSTAL_KEY));
+			if (passive == ArcaneMagicUtils.ForgeCrystal.COAL)
+			{
+				info.setReturnValue((int)(info.getReturnValue() * 1.2f));
+			}
+		}
+	}
+
+	@Inject(at = @At("RETURN"), method = "getAttributeModifiers", cancellable = true)
 	private void getAttributeModifiers(EquipmentSlot slot, CallbackInfoReturnable<Multimap<String, EntityAttributeModifier>> info)
 	{
 		if (getItem() instanceof DaggerItem && getTag() != null && getTag().containsKey(ArcaneMagicConstants.PASSIVE_CRYSTAL_KEY) && slot == EquipmentSlot.HAND_MAIN)
 		{
-			DaggerItem dagger = (DaggerItem)getItem();
+			DaggerItem dagger = (DaggerItem) getItem();
 			Multimap<String, EntityAttributeModifier> map = HashMultimap.create();
 			ArcaneMagicUtils.ForgeCrystal passive = ArcaneMagicUtils.ForgeCrystal.getFromID(getTag().getString(ArcaneMagicConstants.PASSIVE_CRYSTAL_KEY));
 
 			if (passive == ArcaneMagicUtils.ForgeCrystal.GOLD)
 			{
-				map.put(EntityAttributes.ATTACK_SPEED.getId(), new EntityAttributeModifier(DaggerItem.getSpeedModifier(), "Weapon modifier", (double)dagger.getSpeed() + 1, EntityAttributeModifier.Operation.ADDITION));
+				map.put(EntityAttributes.ATTACK_SPEED.getId(), new EntityAttributeModifier(DaggerItem.getSpeedModifier(), "Weapon modifier", (double) dagger.getSpeed() + 0.5d, EntityAttributeModifier.Operation.ADDITION));
 
 			} else
 			{
@@ -45,7 +60,7 @@ public abstract class ItemStackMixin
 			}
 			if (passive == ArcaneMagicUtils.ForgeCrystal.REDSTONE)
 			{
-				map.put(EntityAttributes.ATTACK_DAMAGE.getId(), new EntityAttributeModifier(DaggerItem.getDamageModifier(), "Weapon modifier", (double)10, EntityAttributeModifier.Operation.ADDITION));
+				map.put(EntityAttributes.ATTACK_DAMAGE.getId(), new EntityAttributeModifier(DaggerItem.getDamageModifier(), "Weapon modifier", (double) dagger.getWeaponDamage() + 1, EntityAttributeModifier.Operation.ADDITION));
 			} else
 			{
 				map.put(EntityAttributes.ATTACK_DAMAGE.getId(), new EntityAttributeModifier(DaggerItem.getDamageModifier(), "Weapon modifier", dagger.getWeaponDamage(), EntityAttributeModifier.Operation.ADDITION));
