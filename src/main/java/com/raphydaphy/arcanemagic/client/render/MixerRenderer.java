bd6bbb192552b4f346d36750da26209c0b3cba17
Raph Hennessy <raph.hennessy@gmail.com>
2019-03-17 14:31:44 +1100

    Fix mixer tank rendering

--- src/main/java/com/raphydaphy/arcanemagic/client/render/MixerRenderer.java
+++ src/main/java/com/raphydaphy/arcanemagic/client/render/MixerRenderer.java
@@ -14,12 +14,16 @@ import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.render.block.BiomeColors;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.lwjgl.opengl.GL11;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MixerRenderer extends BlockEntityRenderer<MixerBlockEntity>
 {
@@ -40,6 +44,8 @@ public class MixerRenderer extends BlockEntityRenderer<MixerBlockEntity>
 			new RenderUtils.TextureBounds(32, 10, 22, 22, 32, 32), // South
 			new RenderUtils.TextureBounds(32, 10, 22, 22, 32, 32), // West
 			new RenderUtils.TextureBounds(32, 10, 22, 22, 32, 32)}; // East
+
+	private static List<MixerRenderInstance> renderQueue = new ArrayList<>();
 
 	public void render(MixerBlockEntity entity, double renderX, double renderY, double renderZ, float partialTicks, int destroyStage)
 	{
@@ -68,59 +74,113 @@ public class MixerRenderer extends BlockEntityRenderer<MixerBlockEntity>
 				}
 			} else
 			{
-
-				GlStateManager.pushMatrix();
-				MinecraftClient.getInstance().getTextureManager().bindTexture(tankTexture);
-				GlStateManager.enableBlend();
-				GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-				GlStateManager.depthMask(false);
-				GlStateManager.disableCull();
-				GlStateManager.enableAlphaTest();
-				GlStateManager.alphaFunc(GL11.GL_GREATER, 0.003921569F);
-				Tessellator tess = Tessellator.getInstance();
-				BufferBuilder builder = tess.getBufferBuilder();
-
-				double pixel = 1d / 16d;
-
-				builder.begin(GL11.GL_QUADS, VertexFormats.POSITION_UV_COLOR_NORMAL);
-				RenderUtils.renderBox(builder, renderX + pixel * 2, renderY, renderZ + pixel * 2, renderX + pixel * 14, renderY + pixel * 14, renderZ + pixel * 14, outer, new int[]{1, 1, 1, 1, 1, 1});
-				RenderUtils.renderBox(builder, renderX + pixel * 3, renderY + pixel * 1, renderZ + pixel * 3, renderX + pixel * 13, renderY + pixel * 13, renderZ + pixel * 13, 255, 255, 255, 128, inner, new int[]{1, 1, 1, 1, 1, 1});
-
-				tess.draw();
-				int water = 0;
-				FluidInstance[] fluids = entity.getFluids(null);
-				if (fluids.length >= 1 && fluids[0] != null && fluids[0].getFluid() == Fluids.WATER)
-				{
-					water = (int) (fluids[0].getAmount() / (float) MixerBlockEntity.MAX_FLUID * 12);
-				}
-				if (water > 0)
-				{
-					int frame = 0;
-
-					World world;
-					if ((world = entity.getWorld()) != null)
-					{
-						frame = (int) ((world.getTime() / 2) % 32);
-					}
-					RenderUtils.TextureBounds[] waterBounds = {
-							new RenderUtils.TextureBounds(0, frame * 16, 12, 10 + frame * 16, 16, 512), // Bottom
-							new RenderUtils.TextureBounds(0, frame * 16, 12, 10 + frame * 16, 16, 512), // Top
-							new RenderUtils.TextureBounds(0, frame * 16, 12, water + frame * 16, 16, 512), // North
-							new RenderUtils.TextureBounds(0, frame * 16, 12, water + frame * 16, 16, 512), // South
-							new RenderUtils.TextureBounds(0, frame * 16, 12, water + frame * 16, 16, 512), // West
-							new RenderUtils.TextureBounds(0, frame * 16, 12, water + frame * 16, 16, 512)}; // East
-
-					MinecraftClient.getInstance().getTextureManager().bindTexture(waterTexture);
-					builder.begin(GL11.GL_QUADS, VertexFormats.POSITION_UV_COLOR_NORMAL);
-					int waterColor = BiomeColors.waterColorAt(getWorld(), entity.getPos());
-					RenderUtils.renderBox(builder, renderX + pixel * 3, renderY + pixel * 1, renderZ + pixel * 3, renderX + pixel * 13, renderY + pixel * (1 + water), renderZ + pixel * 13, (waterColor >> 16 & 255), (waterColor >> 8 & 255), (waterColor & 255), 255, waterBounds, new int[]{1, 1, 1, 1, 1, 1});
-
-					tess.draw();
-				}
-				GlStateManager.depthMask(true);
-
-				GlStateManager.popMatrix();
+				renderQueue.add(new MixerRenderInstance(entity, renderX, renderY, renderZ));
 			}
+		}
+	}
+
+	public static void renderMixers()
+	{
+		if (renderQueue.size() > 0)
+		{
+			PlayerEntity player = MinecraftClient.getInstance().player;
+
+			renderQueue.sort((one, two) ->
+			{
+				BlockPos posOne = one.entity.getPos();
+				BlockPos posTwo = two.entity.getPos();
+
+				double distanceOne = Math.abs(posOne.getX() - player.x) * Math.abs(posOne.getY() - player.y) * Math.abs(posOne.getZ() - player.z);
+				double distanceTwo = Math.abs(posTwo.getX() - player.x) * Math.abs(posTwo.getY() - player.y) * Math.abs(posTwo.getZ() - player.z);
+
+				return Double.compare(distanceTwo, distanceOne);
+			});
+
+			GlStateManager.pushMatrix();
+
+			GuiLighting.enable();
+			GlStateManager.enableBlend();
+			GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+			GlStateManager.depthMask(false);
+			GlStateManager.disableCull();
+			GlStateManager.enableAlphaTest();
+			GlStateManager.alphaFunc(GL11.GL_GREATER, 0.003921569F);
+
+			for (MixerRenderInstance instance : renderQueue)
+			{
+				renderTank(instance.entity, instance.renderX, instance.renderY, instance.renderZ);
+			}
+
+			GlStateManager.enableCull();
+			GlStateManager.depthMask(true);
+			GlStateManager.popMatrix();
+			renderQueue.clear();
+		}
+	}
+
+	private static void renderTank(MixerBlockEntity entity, double renderX, double renderY, double renderZ)
+	{
+		GlStateManager.pushMatrix();
+
+		MinecraftClient.getInstance().getTextureManager().bindTexture(tankTexture);
+		Tessellator tess = Tessellator.getInstance();
+		BufferBuilder builder = tess.getBufferBuilder();
+
+		double pixel = 1d / 16d;
+
+		builder.begin(GL11.GL_QUADS, VertexFormats.POSITION_UV_COLOR_NORMAL);
+		RenderUtils.renderBox(builder, renderX + pixel * 2, renderY, renderZ + pixel * 2, renderX + pixel * 14, renderY + pixel * 14, renderZ + pixel * 14, outer, new int[]{1, 1, 1, 1, 1, 1});
+		RenderUtils.renderBox(builder, renderX + pixel * 3, renderY + pixel * 1, renderZ + pixel * 3, renderX + pixel * 13, renderY + pixel * 13, renderZ + pixel * 13, 255, 255, 255, 128, inner, new int[]{1, 1, 1, 1, 1, 1});
+
+		tess.draw();
+		int water = 0;
+		FluidInstance[] fluids = entity.getFluids(null);
+		if (fluids.length >= 1 && fluids[0] != null && fluids[0].getFluid() == Fluids.WATER)
+		{
+			water = (int) (fluids[0].getAmount() / (float) MixerBlockEntity.MAX_FLUID * 12);
+		}
+		if (water > 0)
+		{
+			int frame = 0;
+
+			World world;
+			if ((world = entity.getWorld()) != null)
+			{
+				frame = (int) ((world.getTime() / 2) % 32);
+			}
+			RenderUtils.TextureBounds[] waterBounds = {
+					new RenderUtils.TextureBounds(0, frame * 16, 12, 10 + frame * 16, 16, 512), // Bottom
+					new RenderUtils.TextureBounds(0, frame * 16, 12, 10 + frame * 16, 16, 512), // Top
+					new RenderUtils.TextureBounds(0, frame * 16, 12, water + frame * 16, 16, 512), // North
+					new RenderUtils.TextureBounds(0, frame * 16, 12, water + frame * 16, 16, 512), // South
+					new RenderUtils.TextureBounds(0, frame * 16, 12, water + frame * 16, 16, 512), // West
+					new RenderUtils.TextureBounds(0, frame * 16, 12, water + frame * 16, 16, 512)}; // East
+
+			MinecraftClient.getInstance().getTextureManager().bindTexture(waterTexture);
+			builder.begin(GL11.GL_QUADS, VertexFormats.POSITION_UV_COLOR_NORMAL);
+			int waterColor = BiomeColors.waterColorAt(entity.getWorld(), entity.getPos());
+			RenderUtils.renderBox(builder, renderX + pixel * 3, renderY + pixel * 1, renderZ + pixel * 3, renderX + pixel * 13, renderY + pixel * (1 + water), renderZ + pixel * 13, (waterColor >> 16 & 255), (waterColor >> 8 & 255), (waterColor & 255), 255, waterBounds, new int[]{1, 1, 1, 1, 1, 1});
+
+			tess.draw();
+		}
+
+		GlStateManager.popMatrix();
+	}
+
+	private class MixerRenderInstance
+	{
+		MixerBlockEntity entity;
+
+		double renderX;
+		double renderY;
+		double renderZ;
+
+		MixerRenderInstance(MixerBlockEntity entity, double renderX, double renderY, double renderZ)
+		{
+			this.entity = entity;
+			this.renderX = renderX;
+			this.renderY = renderY;
+			this.renderZ = renderZ;
 		}
 	}
 }
--- src/main/java/com/raphydaphy/arcanemagic/core/client/GameRendererMixin.java
+++ src/main/java/com/raphydaphy/arcanemagic/core/client/GameRendererMixin.java
@@ -1,6 +1,7 @@
 package com.raphydaphy.arcanemagic.core.client;
 
 import com.raphydaphy.arcanemagic.client.particle.ParticleRenderer;
+import com.raphydaphy.arcanemagic.client.render.MixerRenderer;
 import net.minecraft.class_4184;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
@@ -18,8 +19,9 @@ public class GameRendererMixin
 	@Shadow @Final private class_4184 field_18765; // Camera
 
 	@Inject(at = @At(value = "INVOKE_STRING", args = "ldc=hand", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V"), method = "renderCenter")
-	private void renderCenter(float partialTicks, long finishTimeNano, CallbackInfo info)
+	private void renderAfterWorld(float partialTicks, long finishTimeNano, CallbackInfo info)
 	{
 		ParticleRenderer.INSTANCE.render(partialTicks, field_18765);
+		MixerRenderer.renderMixers();
 	}
 }
