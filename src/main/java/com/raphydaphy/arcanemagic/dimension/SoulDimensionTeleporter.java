b83dfbf7e08296640a54af18b4fc5662df13f32a
Raph Hennessy <raph.hennessy@gmail.com>
2019-04-02 16:43:31 +1100

    This will delete

---
+++ src/main/java/com/raphydaphy/arcanemagic/dimension/SoulDimensionTeleporter.java
@@ -0,0 +1,12 @@
+package com.raphydaphy.arcanemagic.dimension;
+
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.PortalForcer;
+
+public class SoulDimensionTeleporter extends PortalForcer
+{
+	public SoulDimensionTeleporter(ServerWorld serverWorld_1)
+	{
+		super(serverWorld_1);
+	}
+}
